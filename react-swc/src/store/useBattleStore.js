46fa7093accdb020635e4e6e60afa9d0bdfa714d
lexus101 <89865282+lexus101@users.noreply.github.com>
2026-03-05 20:20:18 -0800

    Updated values

--- react-swc/src/store/useBattleStore.js
+++ react-swc/src/store/useBattleStore.js
@@ -7,38 +7,45 @@ function buildBattleState(set) {
   const player = new Player('player', 20, null, [
     "STRIKE",
     "STRIKE",
-    "QUICK_STRIKE",
-    "DOUBLE_STRIKE",
-    "HEAVY_STRIKE",
-    "FIVEFOLD_STRIKE",
-    "DEFEND",
-    "RUSH",
     "PATCH_UP",
+    "DEFEND",
     "FOCUS",
-    "CHARGE",
-    "ICE_SHARD",
-    "LIGHTNING_STRIKE",
-    "WATER_FLOW",
-    "FIREBALL"
+    "RUSH"
+
   ]);
 
   const g1 = createEnemy('GOBLIN');
-  const g2 = createEnemy('GOBLIN');
 
+  const g2 = createEnemy('GOBLIN');
+  const g3 = createEnemy('GOBLIN');
+  
   const s1 = createEnemy('SKELETON');
   const s2 = createEnemy('SKELETON');
   const s3 = createEnemy('SKELETON');
+
   const s4 = createEnemy('SKELETON');
   const s5 = createEnemy('SKELETON');
-
   const s6 = createEnemy('SKELETON');
-  const v1 = createEnemy('VAMPIRE_BAT');
   const s7 = createEnemy('SKELETON');
+  const s8 = createEnemy('SKELETON');
+  const s9 = createEnemy('SKELETON');
+
+
+  const s10 = createEnemy('SKELETON');
+  const v1 = createEnemy('VAMPIRE_BAT');
+  const s11 = createEnemy('SKELETON');
+
+  const v2 = createEnemy('VAMPIRE_BAT');
+  const v3 = createEnemy('VAMPIRE_BAT');
+  const v4 = createEnemy('VAMPIRE_BAT');
 
   const enemies = [
-    [g1, g2],
-    [s1, s2, s3, s4, s5],
-    [s6, v1, s7],
+    [g1,v2],
+    [g2,g3],
+    [s1, s2, s3,], 
+    [ s4, s5,s6,s7,s8,s9],
+    [s10, v1, s11],
+    [v2,v3,v4],
   ];
 
   const restartBattle = () => {
